67c39530dcddbb1e29f6eb788e008b7a4ab206d3
rcantor <roycantor@gmail.com>
2017-04-24 00:16:43 +0800

    Add files via upload

--- twitter_histogram.php
+++ twitter_histogram.php
@@ -18,8 +18,8 @@ $settings = array(
 );
 $url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
 $requestMethod = "GET";
-if (isset($_GET['user']))  {$user = $_GET['user'];}  else {$user  = "iagdotme";}
-if (isset($_GET['count'])) {$count = $_GET['count'];} else {$count = 100;}
+if (isset($_GET['user']))  {$user = $_GET['user'];}  else {$user  = "bbcdoctorwho";}
+if (isset($_GET['count'])) {$count = $_GET['count'];} else {$count = 500;}
     
     
 $getfield = "?screen_name=$user&count=$count";
@@ -28,7 +28,11 @@ $string = json_decode($twitter->setGetfield($getfield)
 ->buildOauth($url, $requestMethod)
 ->performRequest(),$assoc = TRUE);
 //if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
+    
+    echo "<h2>Tweets by ".$user."</h2>";
+    
 foreach($string as $items)
+    
     {
         $time_created = strtotime($items['created_at']);
         //echo "Time and Date of Tweet: ".$items['created_at']."<br />";
